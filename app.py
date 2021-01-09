56386fe1db81941426fc6f1dd30c66a35ee872a6
Jordimuletestaras <46597590+Jordimuletestaras@users.noreply.github.com>
2021-01-09 22:12:18 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import werkzeug
+import pafy
+import lxml
+from lxml import etree
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,22 +12,28 @@ api = Api(app)
 class Menu(Resource):
     def get(self):
 
-        return {'Menu': 'use /1 to VideoCoding /2 to AudioCoding'}
+        return {'Menu': 'use /VideoDuration to get the duration of a video from youtube, /Diagram to see the perceptual coder diagram'}
 
 class VideoCoding(Resource):
-    def get(self):
-        x=100
-        return {x}
+    def get(self, id):
+        
+        url = 'https://www.youtube.com/watch?v=' + id
+        
+        video = pafy.new(url)
+
+        #youtube = etree.HTML(urllib.urlopen(url).read()) 
+        #join(video_title)
+        return {'video': {'title':'hhhh' , 'duration': video.length}}, 200
 
 class AudioCoding(Resource):
     def get(self):
 
-        return {'VideoCoding': 'file:///C:/Users/nuriaestaras/Desktop/jordi/provahtml.html'}
+        return {'image': 'https://github.com/Jordimuletestaras/API/blob/main/diagrama.JPG'}
 
 
 api.add_resource(Menu, '/')
-api.add_resource(VideoCoding, '/1')
-api.add_resource(AudioCoding, '/2')
+api.add_resource(VideoCoding, '/VideoDuration/<string:id>')
+api.add_resource(AudioCoding, '/Diagram')
 
 if __name__ == '__main__':
     app.run(debug=True)
